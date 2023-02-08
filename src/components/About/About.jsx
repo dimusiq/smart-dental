c5b58bd1946a9c17a95b4634744515abf53ff044
dimusiq <dumojke@gmail.com>
2023-02-08 23:20:54 +0500

    about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import Button from '../Button/Button';
 import styles from '../About/About.module.scss';
 import { about } from '../../assets/index';
 
 const About = () => {
-	return (
-		<div className={styles.content}>
-			<img
-				src={about}
-				alt='about'
-				className={styles.content__img}
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.content}>
+      <img
+        src={about}
+        alt='about'
+        className={styles.content__img}
+      />
+      <div className={styles.window}>
+        <h1 className={styles.window__title}>
+          Бесплатная консультация включает в себя:
+        </h1>
+        <ul className={styles.window__title__list}>
+          <li>Первичный осмотр</li>
+          <li>Снимки</li>
+          <li>План лечения</li>
+        </ul>
+        <Button className={styles.window__button}>
+          Записаться
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default About;
