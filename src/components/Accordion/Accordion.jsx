1c4c2ac1686ab4dc2a019b54006c1a2d08bcfa77
dimusiq <dumojke@gmail.com>
2023-02-14 23:17:00 +0500

    added logic to accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -3,13 +3,24 @@ import styles from './Accordion.module.scss';
 import { accordionData } from '../../constants';
 
 const Accordion = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const accordionHandler = () => {
+    setIsActive((prev) => !prev);
+  };
   return (
     <div className={styles.container}>
       <h2 className={styles.title}></h2>
       <div className={styles.accordion}>
         {accordionData.map((item, i) => (
-          <div key={i} className={styles.accordion__title}>
-            {item.title}
+          <div onClick={accordionHandler}>
+            <div
+              key={i}
+              className={styles.accordion__title}
+            >
+              {item.title}
+              {isActive && <div>{item.content}</div>}
+            </div>
           </div>
         ))}
       </div>
