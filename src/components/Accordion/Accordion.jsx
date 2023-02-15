d663aeb95a7e900ea6c7fd15477dcbde2c377ab2
dimusiq <dumojke@gmail.com>
2023-02-15 22:51:08 +0500

    minor fixes

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -3,24 +3,32 @@ import styles from './Accordion.module.scss';
 import { accordionData } from '../../constants';
 
 const Accordion = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(null);
 
-  const accordionHandler = () => {
-    setIsActive((prev) => !prev);
+  const accordionHandler = (i) => {
+    if (isActive === i) {
+      return setIsActive(null);
+    }
+    setIsActive(i);
   };
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}></h2>
+      <h2 className={styles.title}>УСЛУГИ</h2>
       <div className={styles.accordion}>
         {accordionData.map((item, i) => (
-          <div onClick={accordionHandler}>
+          <div onClick={() => accordionHandler(i)}>
             <div
               key={i}
               className={styles.accordion__title}
             >
               {item.title}
-              {isActive && <div>{item.content}</div>}
+              <div>{isActive === i ? '-' : '+'}</div>
             </div>
+            {isActive === i && (
+              <div className={styles.accordion__content}>
+                {item.content}
+              </div>
+            )}
           </div>
         ))}
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -63,24 +63,27 @@ export const actions = [
 export const accordionData = [
   {
     title: 'Консультация и диагностика',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-    laborum cupiditate possimus labore, hic temporibus velit dicta earum
-    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-    voluptatem.`,
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. `,
   },
   {
     title: 'Удаление зуба ',
-    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-    reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-    quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-    pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-    quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-    Repudiandae, mollitia id reprehenderit a ab odit!`,
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. `,
   },
   {
     title: 'Профгигиена  полости рта',
     content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-    quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-    dolor ut sequi minus iste? Quas?`,
+`,
+  },
+  {
+    title: 'Хирургия',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. .`,
+  },
+  {
+    title: 'Отбеливание зубов',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. `,
+  },
+  {
+    title: 'Установка имплантата',
+    content: `Sapiente expedita hic obcaecati, laboriosam similique omnis`,
   },
 ];
