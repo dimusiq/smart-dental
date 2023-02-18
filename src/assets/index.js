11350446f03ea6f1f73cc75f99173c0fd484c231
dimusiq <dumojke@gmail.com>
2023-02-18 22:01:10 +0500

    code fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,6 @@ import logo from './logo.svg';
 import tel from './tel.svg';
 import about from './about.png';
 import smile from './smile.png';
+import list from './list.svg';
 
-export { logo, tel, about, smile };
+export { logo, tel, about, smile, list };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { list } from '../../assets';
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
 import styles from './Form.module.scss';
@@ -18,18 +19,31 @@ const Form = ({ onHideForm }) => {
               Бесплатная консультация включает в себя:
             </div>
             <ul className={styles.form__list}>
+              <img src={list} alt='list'></img>
               <li>Первичный осмотр</li>
               <li>Снимки</li>
               <li>План лечения</li>
             </ul>
           </div>
-          <div className={styles.form__input}>
-            <input type='text' placeholder='Имя' />
-            <input type='text' placeholder='Телефон' />
-            <Button>Записаться</Button>
-            <div>
-              Нажимая на кнопку, я соглашаюсь на обработку
-              персональных данных
+          <div className={styles.form__container}>
+            <input
+              className={styles.form__container__input}
+              type='text'
+              placeholder='Имя'
+            />
+            <input
+              className={styles.form__container__input}
+              type='text'
+              placeholder='Телефон'
+            />
+            <Button className={styles.form__btn}>
+              Записаться
+            </Button>
+            <div className={styles.form__info}>
+              <p>
+                Нажимая на кнопку, я соглашаюсь на обработку
+                персональных данных
+              </p>
             </div>
           </div>
         </div>
